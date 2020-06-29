555bc9ad3a0e881437962c9aadf808b93dddc99f
Tony Raubenheimer <tonyraubenheimer@gmail.com>
2020-06-28 23:46:13 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -40,13 +40,12 @@ learning_rate = .01
 num_runs = 5
 
 def timer(func): #from https://realpython.com/lessons/timing-functions-decorators/
-    """Print the runtime of the decorated function"""
     @functools.wraps(func)
     def wrapper_timer(*args, **kwargs):
-        start_time = time.perf_counter()  # 1
+        start_time = time.perf_counter()  
         value = func(*args, **kwargs)
-        end_time = time.perf_counter()  # 2
-        run_time = end_time - start_time  # 3
+        end_time = time.perf_counter()  
+        run_time = end_time - start_time  
         print(f"Finished {func.__name__!r} in {run_time:.1f} secs")
         return value
     return wrapper_timer
